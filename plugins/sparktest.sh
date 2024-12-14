9e2de9abf6fd73c3aa4fabd2efe2a5b97fbf2884
brianbok <brianbok97@gmail.com>
2024-12-14 22:53:52 +0900

    bash operator with no networks

--- plugins/sparktest.sh
+++ plugins/sparktest.sh
@@ -3,5 +3,6 @@
 script=$1
 echo "${script}"
 echo "start spark submit with bash operator"
-spark-submit \
+
+docker exec -it rankingflow-spark-master-1 spark-submit \
     --master spark://spark-master:7077 jobs/${script}
