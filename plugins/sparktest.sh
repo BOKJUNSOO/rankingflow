ffb0317f87bb057107631a543bc4c379faf91b05
brianbok <brianbok97@gmail.com>
2024-12-15 00:19:27 +0900

    airflow bash operator에 docker 명령어는 사용이 안된다

--- plugins/sparktest.sh
+++ plugins/sparktest.sh
@@ -4,4 +4,4 @@ script=$1
 echo "${script}"
 echo "start spark submit with bash operator"
 
-docker exec -it rankingflow-spark-master-1 spark-submit --master spark://spark-master:7077 jobs/${script}
+spark-submit --master spark://spark-master:7077 jobs/${script}
