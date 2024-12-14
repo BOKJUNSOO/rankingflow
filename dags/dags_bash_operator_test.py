93a375e29a80f827de5abe980640960cfc18fe2e
brianbok <brianbok97@gmail.com>
2024-12-15 00:10:00 +0900

    spark job 파일 절대경로로 수정

--- dags/dags_bash_operator_test.py
+++ dags/dags_bash_operator_test.py
@@ -3,6 +3,8 @@ from airflow.operators.bash import BashOperator
 import pendulum
 from datetime import datetime
 
+file_name = "/opt/airflow/jobs/sparktest.py"
+
 with DAG(
     dag_id = "dags_bash_operator_test",
     start_date=pendulum.datetime(2024, 12, 12, tz = "Asia/Seoul"),
@@ -12,7 +14,7 @@ with DAG(
 
     task1 = BashOperator(
         task_id = "task1",
-        bash_command="/opt/airflow/plugins/sparktest.sh sparktest.py"
+        bash_command=f'/opt/airflow/plugins/sparktest.sh {file_name}'
     )
 
     task1
