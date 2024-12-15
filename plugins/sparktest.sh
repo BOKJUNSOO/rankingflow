bd490c856240cba92604ac84d39a417bbc5ea06b
brianbok <brianbok97@gmail.com>
2024-12-15 13:56:53 +0900

    shell script

--- plugins/sparktest.sh
+++ plugins/sparktest.sh
@@ -3,7 +3,8 @@
 script=$1
 echo "${script}"
 echo "start spark submit with bash operator"
-
+echo "${PATH}"
+echo "${SPARK_HOME}"
 spark-submit \
     --master spark://spark-master:7077 \
     ${script}
