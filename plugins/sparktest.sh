61ac0c5dc2f542cf231464ea85d0c8ec60c539d4
brianbok <brianbok97@gmail.com>
2024-12-15 15:48:51 +0900

    shell modify

--- plugins/sparktest.sh
+++ plugins/sparktest.sh
@@ -5,4 +5,5 @@ echo "${script}"
 echo "start spark submit with bash operator"
 echo "${PATH}"
 echo "${SPARK_HOME}"
-docker exec -it rankingflow-spark-master-1 spark-submit --master spark://spark-master:7077 ${script}
+spark-submit \
+    --master spark://spark-master:7077 ${script}
