da4cc9922c92e7eb506d95180bcb002d374a4489
brianbok <brianbok97@gmail.com>
2024-12-15 12:19:42 +0900

    shell script

--- plugins/sparktest.sh
+++ plugins/sparktest.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 script=$1
+JOBNAME=$JOBNAME
 echo "${script}"
 echo "start spark submit with bash operator"
 
-spark-submit --master spark://spark-master:7077 jobs/${script}
+spark-submit \
+    --name ${JOBNAME} \
+    --master spark://spark-master:7077 \
+    ${script}
