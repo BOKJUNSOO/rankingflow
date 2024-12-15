f6965435f2b3915bb889a5a11ccf6919651ef8fb
brianbok <brianbok97@gmail.com>
2024-12-15 13:49:23 +0900

    spark submit shell

--- plugins/sparktest.sh
+++ plugins/sparktest.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 script=$1
-JOBNAME=$JOBNAME
 echo "${script}"
 echo "start spark submit with bash operator"
 
 spark-submit \
-    --name ${JOBNAME} \
     --master spark://spark-master:7077 \
-    ${script}   # /opt/airflow/jobs/pyfile
+    ${script}
