f83cb95c3bf46b195959b5f75c0c1d894e09f710
brianbok <brianbok97@gmail.com>
2024-12-16 19:57:45 +0900

    need spark optimization

--- plugins/sparktest.sh
+++ plugins/sparktest.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 script=$1
-echo "${script}"
+JOBNAME="RefineData"
+echo "Job name is ${JOBNAE}"
+echo "submit this job >> ${script}"
 echo "start spark submit with bash operator"
-echo "${PATH}"
-#echo "${SPARK_HOME}"
+
 spark-submit \
+    --name ${JOBNAME} \
     --master spark://spark-master:7077 ${script}
