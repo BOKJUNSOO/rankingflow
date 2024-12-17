8da785587cdc030e139cf5635cbb6a0a5b3eb23d
brianbok <brianbok97@gmail.com>
2024-12-17 14:43:53 +0900

    return

--- jobs/sparktest.py
+++ jobs/sparktest.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 import datetime
 import sys
-#from common.get_data import get_data
+# from common.get_data import get_data
 spark = (
     SparkSession.builder \
                 .master("local")
@@ -19,8 +19,8 @@ target_date = target_date.strftime('%Y-%m-%d')
 print(f"{target_date}일자의 data를 정제합니다.")
 # 특정 일자의 data가 없으면 common fuction 호출해서 데이터 수집하기
 # spark python 경로 확인
-print("spark module 인식 경로:",sys.path)
-file_path = f"/opt/spark/data/ranking_{target_date}.json"
+# print("spark module 인식 경로:",sys.path)
+file_path = f"/opt/airflow/data/ranking_{target_date}.json"
 
 print(f"{file_path}")
 df = spark.read \
