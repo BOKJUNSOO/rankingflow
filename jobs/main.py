51641e89348cf0dbd5912cb85d9595432ecdf8a8
brianbok <brianbok97@gmail.com>
2024-12-23 18:14:06 +0900

    경로 오타 수정

--- jobs/main.py
+++ jobs/main.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     batch_date= batch_date.strftime("%Y-%m-%d")
     batch_yesterday_date=batch_yesterday_date.strftime("%Y-%m-%d")
 
-    batch_data_path= f"/opt/airflow/data/{batch_date}" # batch일 데이터 경로
-    batch_y_data_path= f"/opt/airflow/data/{batch_yesterday_date}" # batch전날 데이터 경로
+    batch_data_path= f"/opt/airflow/data/ranking_{batch_date}.json" # batch일 데이터 경로
+    batch_y_data_path= f"/opt/airflow/data/ranking_{batch_yesterday_date}.json" # batch전날 데이터 경로
 
     # USER 테이블 생성
     user_df=make_user_dataframe(spark,batch_data_path)
