edeedcbf3f54c19b5b3feb8979531894a1277da6
brianbok <brianbok97@gmail.com>
2024-12-23 21:37:00 +0900

    agg_status 추가 및 각주 추가

--- jobs/spark_common/filter.py
+++ jobs/spark_common/filter.py
@@ -1,7 +1,7 @@
-from spark_common.base import BaseFilter
 import pyspark.sql.functions as F
 
 class DataFrameFilter():
+    # USER 테이블로부터 Class_Status 테이블로 변환시키는 함수
     def agg_status(df):
         filtered_df = df.groupBy("class","date").pivot("status") \
                       .agg(F.count("status"))
