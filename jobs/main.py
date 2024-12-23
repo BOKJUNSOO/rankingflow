62497381878bfa0e64ac1039dc8c6ded1f2e9e56
brianbok <brianbok97@gmail.com>
2024-12-23 18:12:31 +0900

    spark read 객체 생성

--- jobs/main.py
+++ jobs/main.py
@@ -1,18 +1,14 @@
 from pyspark.sql import SparkSession
 from datetime import datetime , timedelta
 from spark_common.base import make_user_dataframe
-import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args()
 
     spark = SparkSession.builder \
                         .master("local") \
                         .appName("Spark_Submit") \
                         .getOrCreate()
                         #.config()
-    args.spark = spark
     # batch일자의 data와 batch 전날 data를 load
     UTC = datetime.now()
     batch_date= UTC + timedelta(hours=9)
@@ -21,11 +17,11 @@ if __name__ == "__main__":
     batch_date= batch_date.strftime("%Y-%m-%d")
     batch_yesterday_date=batch_yesterday_date.strftime("%Y-%m-%d")
 
-    args.batch_data_path= f"/opt/airflow/data/{batch_date}" # batch일 데이터 경로
-    args.batch_y_data_path= f"/opt/airflow/data/{batch_yesterday_date}" # batch전날 데이터 경로
+    batch_data_path= f"/opt/airflow/data/{batch_date}" # batch일 데이터 경로
+    batch_y_data_path= f"/opt/airflow/data/{batch_yesterday_date}" # batch전날 데이터 경로
 
     # USER 테이블 생성
-    user_df=make_user_dataframe(args.spark,args.batch_data_path)
+    user_df=make_user_dataframe(spark,batch_data_path)
     user_df.show(10)
                         
 
--- jobs/spark_common/base.py
+++ jobs/spark_common/base.py
@@ -15,10 +15,10 @@ def pass_spark_dataframe(func):
 
 # spark 객체를 이용하여 file_path에 존재하는 데이터를 읽어와 sparkdataframe을 생성하는 함수수
 def make_spark_dataframe(spark:object, file_path:str)->object:
-    spark_df = spark.read() \
-              .format("json") \
-              .option("multiLine", True) \
-              .load(file_path)
+    spark_df = spark.read \
+                    .format("json") \
+                    .option("multiLine", True) \
+                    .load(file_path)
     print(f"{file_path} data를 load 합니다.")
     return spark_df
 
