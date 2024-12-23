358651a92e8d816ecaaa634aa46f6f530e52a452
brianbok <brianbok97@gmail.com>
2024-12-24 03:52:20 +0900

    class 멤버변수 설정

--- jobs/spark_common/filter.py
+++ jobs/spark_common/filter.py
@@ -1,14 +1,18 @@
 import pyspark.sql.functions as F
 
-class DataFrameFilter():
+class StatusFilter:
+    def __init__(self,df):
+        self.df = df
     # USER 테이블로부터 Class_Status 테이블을 만드는 함수
-    def agg_class_status(self,df):
+    def agg_class_status(self):
+        df = self.df
         filtered_df = df.groupBy("class","date").pivot("status") \
                       .agg(F.count("status"))
         return filtered_df
     
     # BATCH 전날과 BATCH 일의 데이터를 이용해서 AchievementSummary 테이블을 만드는 함수
-    def agg_achive_summary(joined_df):
+    def agg_achive_summary(self):
+        joined_df = self.df
         achive_df=joined_df.select("class","date","status_today","status_yesterday")
         achive_df=achive_df.withColumn("status_change", F.when((achive_df["status_today"] == achive_df["status_yesterday"]) , "stayhere" )
                                                     .when((achive_df["status_today"] != achive_df["status_yesterday"])&(achive_df["status_today"]=="Tallahart"), "welcome to Tallahart")
@@ -21,10 +25,15 @@ class DataFrameFilter():
         achive_df = achive_df.groupBy("class","date","status_change").agg(F.count("*").alias("count"))
         return achive_df
 
+class ExpFilter:
+    def __init__(self,df,exp_df):
+        self.df = df
+        self.exp_df = df
     # BATCH 전날과 BATCH 일의 데이터를 이용해서 user_exp_aggregate 테이블을 만드는 함수
-    def agg_user_exp(self,joined_df,exp_df):
-        pass
-    
+    def agg_user_exp(self):
+        joined_df = self.df
+        exp_df = self.exp_df
+        
     # agg_user_exp의 리턴된 테이블로부터 class_exp_aggregate 테이블을 만드는 함수
     def agg_class_exp(self,df):
         pass
