50e0eb52dbf31af4c15385a1ca15f09748d779ce
brianbok <brianbok97@gmail.com>
2024-12-25 21:51:50 +0900

    --jars 수정

--- plugins/sparksubmit.sh
+++ plugins/sparksubmit.sh
@@ -8,5 +8,5 @@ echo 'start spark submit with bash operator'
 
 spark-submit \
     --name ${JOBNAME} \
-    --jars /opt/bitnami/spark/resources/elasticsearch-spark-30_2.12-7.15.2.jar \
+    --jars /opt/airflow/resources/elasticsearch-spark-30_2.12-7.15.2.jar \
     --master spark://spark-master:7077 ${script}
