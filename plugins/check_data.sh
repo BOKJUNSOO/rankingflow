6660d9f8b560f2868519647ebe86851bb2ef7841
junesoo <brianbok97@gmail.com>
2025-04-10 01:41:29 +0900

    check_data_quilty 마무리

--- plugins/check_data.sh
+++ plugins/check_data.sh
@@ -3,7 +3,7 @@ today_date=$1
 yesterday_date=$2
 echo "${today_date}의 데이터 누락여부를 확인합니다."
 cat /opt/airflow/data/ranking_${today_date} | grep 'please'
-if [ $? -eq 1]; then
+if [ $? -eq 0 ] ; then
     echo "${today_date}의 누락된 데이터가 존재합니다."
 else
     echo "${today_date}의 누락된 데이터가 존재하지 않습니다."
@@ -11,7 +11,7 @@ fi
 
 echo "${yesterday_date}의 데이터 누락여부를 확인합니다."
 cat /opt/airflow/data/ranking_${yesterday_date} | grep 'please'
-if [ $? -eq 1]; then
+if [ $? -eq 0 ] ; then
     echo "${yesterday_date}의 누락된 데이터가 존재합니다."
 else
     echo "${yesterday_date}의 누락된 데이터가 존재하지 않습니다."
