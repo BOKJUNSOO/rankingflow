9b6945364b7cf8a38b32ea77883490c645186401
junesoo <brianbok97@gmail.com>
2025-04-10 17:54:48 +0900

    go

--- plugins/search_data.sh
+++ plugins/search_data.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 index=$1
-date=$2
 
 echo "저장된 인덱스 목록"
 curl -X GET "localhost:9200/_cat/indices?pretty"
 
 echo
-echo "${index}_${date}저장된 데이터"
-curl -X GET localhost:9200/${index}_${date}/_search?pretty
+echo "${index} 의 사냥 데이터"
+curl -X GET "http://localhost:9200/user_exp_*/_search?pretty" \
+	     -H 'Content-Type: application/json' \
+	          -d @- <<EOF
+{
+  "query": {
+    "match": {
+      "character_name": "${index}"
+    }
+  }
+}
+EOF
