1ac5b312c6d8abb80b8a21831081ea1f9f7e83b7
복준수 <brianbok97@gmail.com>
2025-04-24 15:41:47 +0900

    Feat: es 삭제 및 스펙다운

--- plugins/sparksubmit.sh
+++ plugins/sparksubmit.sh
@@ -10,10 +10,11 @@ echo 'start spark submit with bash operator'
 
 spark-submit \
     --name ${JOBNAME} \
-    --jars /opt/airflow/resources/elasticsearch-spark-30_2.12-8.11.1.jar,/opt/airflow/resources/mysql-connector-j-8.0.33.jar \
-    --master local\
+    --jars /opt/airflow/resources/mysql-connector-j-8.0.33.jar \
+    --master local[2]\
     --conf spark.driver.memory=${DRIVER_MEMORY} \
     --conf spark.executor.memory=${EXECUTOR_MEMORY} \
-    --conf spark.num.executors=2\
     --conf spark.eventLog.enabled=true\
     ${script}
+#--conf spark.num.executors=2\
+#/opt/airflow/resources/elasticsearch-spark-30_2.12-8.11.1.jar,
