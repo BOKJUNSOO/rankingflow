a20fd3431fa3dbfbff004c76a479c533150a25ff
복준수 <brianbok97@gmail.com>
2025-05-08 13:57:53 +0900

    Chore: 스크립트 문법

--- plugins/sparksubmit.sh
+++ plugins/sparksubmit.sh
@@ -10,9 +10,9 @@ echo 'start spark submit with bash operator'
 
 spark-submit \
     --name ${JOBNAME} \
-    --jars /opt/airflow/resources/elasticsearch-spark-30_2.12-8.11.1.jar, /opt/airflow/resources/mysql-connector-j-8.0.33.jar \
-    --master local[2]\
+    --jars /opt/airflow/resources/elasticsearch-spark-30_2.12-8.11.1.jar,/opt/airflow/resources/mysql-connector-j-8.0.33.jar \
+    --master local[2] \
     --conf spark.driver.memory=${DRIVER_MEMORY} \
     --conf spark.executor.memory=${EXECUTOR_MEMORY} \
-    --conf spark.eventLog.enabled=true\
+    --conf spark.eventLog.enabled=true \
     ${script}
