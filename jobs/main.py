5d3c2c7a681e261883b2b7615bf7e64f3adf22a4
복준수 <brianbok97@gmail.com>
2025-05-08 13:50:51 +0900

    Feat: build elastic search and kibana container

--- jobs/main.py
+++ jobs/main.py
@@ -61,11 +61,11 @@ def main():
 
     # save_data to elasticSearch
     print("write spark data frame at ES database..")
-    #save_to_elastic_search=spark_common.ElasticSearch("http://es:9200")
-    #save_to_elastic_search.write(class_status_df,f"class_status_{batch_date}")
-    #save_to_elastic_search.write(achievement_summary_df,f"achievement_summary_{batch_date}")
-    #save_to_elastic_search.write(user_exp_agg_df,f"user_exp_{batch_date}")
-    #save_to_elastic_search.write(class_exp_df,f"class_exp_{batch_date}")
+    save_to_elastic_search=spark_common.ElasticSearch("http://es:9200")
+    save_to_elastic_search.write(class_status_df,f"class_status_{batch_date}")
+    save_to_elastic_search.write(achievement_summary_df,f"achievement_summary_{batch_date}")
+    save_to_elastic_search.write(user_exp_agg_df,f"user_exp_{batch_date}")
+    save_to_elastic_search.write(class_exp_df,f"class_exp_{batch_date}")
     print("Done!")
     #
     ## save_data to MySQL
